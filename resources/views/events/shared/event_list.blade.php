f19145d63a137287f6eb9da87a78100b46791723
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-06-03 13:29:24 +0800

    add wishlist button

--- resources/views/events/shared/event_list.blade.php
+++ resources/views/events/shared/event_list.blade.php
@@ -1,8 +1,18 @@
 @php
-    /** @var \Illuminate\Database\Eloquent\Collection|\App\Models\Venue[] $events /
- /* @var ?string $noEventsMessage */
+    /** @var \Illuminate\Database\Eloquent\Collection|\App\Models\Venue[] $events */
+    /* @var ?string $noEventsMessage */
     $showVisibility = $showVisibility ?? true;
 @endphp
+
+<style>
+    .wishlist-button .fa-heart {
+        color: black;
+    }
+
+    .wishlist-button.added .fa-heart {
+        color: red;
+    }
+</style>
 
 @if ($events->count() === 0)
     @isset($noEventsMessage)
@@ -22,7 +32,7 @@
                 <a href="{{ route('events.show', $service->slug) }}" class="list-group-item list-group-item-action"
                     style="margin-bottom: 20px;">
                     <div class="row">
-                        <div style="text-align: right" class="col">
+                        <div class="col d-flex justify-content-between align-items-center">
                             <div>
                                 @for ($i = 0; $i < $service->service_rating; $i++)
                                     <span>&#9733;</span> <!-- this is a star character -->
@@ -31,6 +41,8 @@
                                     <span>&#9734;</span> <!-- this is an empty star character -->
                                 @endfor
                             </div>
+                            <button style="background: none; border: none;" class="wishlist-button"
+                                data-service-id="{{ $service->id }}"><i class="fa fa-heart"></i></button>
                         </div>
                     </div>
                     @if ($service->images->count() > 0)
@@ -55,10 +67,6 @@
                             </button>
                         </div>
                     @endif
-                    {{-- <div>
-                    <i class="fa fa-fw fa-clock"></i>
-                    @include('events.shared.event_dates')
-                    </div> --}}
                     <div>
                         <i class="fa fa-fw fa-location-pin"></i>
                         {{ $service->location->nameOrAddress }}
@@ -75,10 +83,30 @@
                     <div class="text-muted">
                         Price from RM{{ $service->min_price }}
                     </div>
-
-                    {{-- <a href="{{ route('chats.index', $service->id) }}" class="btn btn-success">{{ __('Chat') }}</a> --}}
                 </a>
             @endcan
         @endforeach
     </div>
 @endif
+<script>
+    window.addEventListener('DOMContentLoaded', (event) => {
+        const wishlistButtons = document.querySelectorAll('.wishlist-button');
+
+        wishlistButtons.forEach(button => {
+            button.addEventListener('click', function(event) {
+                event.preventDefault();
+                const serviceId = this.getAttribute('data-service-id');
+
+                if (this.classList.contains('added')) {
+                    console.log("Removed from wishlist: " + serviceId);
+                    alert("Removed from wishlist");
+                    this.classList.remove('added');
+                } else {
+                    console.log("Added to wishlist: " + serviceId);
+                    alert("Added to wishlist");
+                    this.classList.add('added');
+                }
+            });
+        });
+    });
+</script>
